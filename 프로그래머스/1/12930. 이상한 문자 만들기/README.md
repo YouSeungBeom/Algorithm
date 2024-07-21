c77235327dd4e732a93fbdcb31195dd1bdff436d
YouSeungBeom <162657946+YouSeungBeom@users.noreply.github.com>
2024-07-22 01:35:59 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.26 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/README.md
+++ 프로그래머스/1/12930. 이상한 문자 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 이상한 문자 만들기 - 12930 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12930#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12930#) 
 
 ### 성능 요약
 
-메모리: 78.6 MB, 시간: 0.09 ms
+메모리: 71.4 MB, 시간: 0.26 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 22일 01:29:57
+2024년 07월 22일 01:35:57
 
 ### 문제 설명
 
--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,22 +1,22 @@
 class Solution {
     public String solution(String s) {
+        String[] str = s.split(" ", -1);
         StringBuilder answer = new StringBuilder();
-        boolean even = true;
         
-        for (char c : s.toCharArray()) {
-            if (c == ' ') {
-                answer.append(c);
-                even = true;
-            } else {
-                if (even) {
-                    answer.append(Character.toUpperCase(c));
+        for (int i = 0; i < str.length; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < str[i].length(); j++) {
+                if (j % 2 == 0) {
+                    sb.append(Character.toUpperCase(str[i].charAt(j)));
                 } else {
-                    answer.append(Character.toLowerCase(c));
+                    sb.append(Character.toLowerCase(str[i].charAt(j)));
                 }
-                even = !even;
+            }
+            answer.append(sb);
+            if (i < str.length - 1) {
+                answer.append(" ");
             }
         }
-        
         return answer.toString();
     }
 }
