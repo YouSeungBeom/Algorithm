01871bb7f8710e5ba240802989b1a936c457da21
YouSeungBeom <162657946+YouSeungBeom@users.noreply.github.com>
2024-07-23 01:10:33 +0900

    Update and rename 최적화 코드.java to 기사단원의 무기(최적화).java
    정확성  테스트
    테스트 1 〉	통과 (1.99ms, 75.2MB)
    테스트 2 〉	통과 (0.58ms, 74.6MB)
    테스트 3 〉	통과 (0.25ms, 92.2MB)
    테스트 4 〉	통과 (1.08ms, 72.8MB)
    테스트 5 〉	통과 (0.19ms, 77.9MB)
    테스트 6 〉	통과 (2.01ms, 73.3MB)
    테스트 7 〉	통과 (0.89ms, 71.2MB)
    테스트 8 〉	통과 (0.62ms, 75.2MB)
    테스트 9 〉	통과 (2.87ms, 76.2MB)
    테스트 10 〉	통과 (0.26ms, 73.4MB)
    테스트 11 〉	통과 (6.63ms, 93.9MB)
    테스트 12 〉	통과 (6.53ms, 70.5MB)
    테스트 13 〉	통과 (9.17ms, 73.2MB)
    테스트 14 〉	통과 (10.24ms, 80.3MB)
    테스트 15 〉	통과 (7.76ms, 76.6MB)
    테스트 16 〉	통과 (6.98ms, 76.4MB)
    테스트 17 〉	통과 (0.02ms, 75.6MB)
    테스트 18 〉	통과 (6.81ms, 77.5MB)
    테스트 19 〉	통과 (0.87ms, 76.4MB)
    테스트 20 〉	통과 (0.49ms, 74MB)
    테스트 21 〉	통과 (0.02ms, 75.8MB)
    테스트 22 〉	통과 (0.03ms, 72MB)
    테스트 23 〉	통과 (0.01ms, 78.3MB)
    테스트 24 〉	통과 (6.70ms, 74.9MB)
    테스트 25 〉	통과 (6.55ms, 73.8MB)
    테스트 26 〉	통과 (0.28ms, 78.2MB)
    테스트 27 〉	통과 (0.17ms, 81.8MB)
    채점 결과
    정확성: 100.0
    합계: 100.0 / 100.0

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기(최적화).java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기(최적화).java
@@ -3,14 +3,11 @@ class Solution {
         int answer = 0;
         int[] count = new int[number + 1];
         
-        // Calculate the number of divisors for each number from 1 to `number`
         for (int i = 1; i <= number; i++) {
             for (int j = 1; j <= number / i; j++) {
                 count[i * j]++;
             }
         }
-        
-        // Sum the appropriate values based on the number of divisors and the limit
         for (int i = 1; i <= number; i++) {
             if (count[i] > limit) {
                 answer += power;
@@ -18,7 +15,6 @@ class Solution {
                 answer += count[i];
             }
         }
-        
         return answer;
     }
 }
