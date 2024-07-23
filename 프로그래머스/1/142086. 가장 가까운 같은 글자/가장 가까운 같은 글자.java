b41f19d4ae48d9eec34193845d17df3b2abdbb1f
YouSeungBeom <162657946+YouSeungBeom@users.noreply.github.com>
2024-07-23 17:45:26 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 67.34 ms, Memory: 91.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[s.length()];
+        
+        for(int i = 0; i < s.length(); i++) {
+            int temp = 0;
+            
+            for(int j = 0; j < i; j++) {
+                if(s.charAt(i) == s.charAt(j)) {
+                    answer[i] = i - j;
+                }
+            }
+            if(answer[i] == 0) {
+                answer[i] = -1;
+            }
+        }
+        return answer;
+    }
+}
