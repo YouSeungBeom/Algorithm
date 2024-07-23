d6586d0bbb652773cdb1c88ecc775e6bad954d40
YouSeungBeom <162657946+YouSeungBeom@users.noreply.github.com>
2024-07-23 18:59:19 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 25.15 ms, Memory: 93.9 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,20 +1,13 @@
 class Solution {
     public String solution(int[] food) {
-        StringBuilder answer = new StringBuilder();
-        for(int i = 1; i < food.length; i++) {
-            food[i] = food[i]/2;
-        }
-        for(int i = 1; i < food.length; i++) {
-            for(int j = 0; j < food[i]; j++) {
-                answer.append(i);
+        String answer = "0";
+
+        for (int i = food.length - 1; i > 0; i--) {
+            for (int j = 0; j < food[i] / 2; j++) {
+                answer = i + answer + i; 
             }
         }
-        answer.append(0);
-        for(int i = food.length - 1; i >= 1; i--) {
-            for(int j = 0; j < food[i]; j++) {
-                answer.append(i);
-            }
-        }
-        return answer.toString();
+
+        return answer;
     }
 }
