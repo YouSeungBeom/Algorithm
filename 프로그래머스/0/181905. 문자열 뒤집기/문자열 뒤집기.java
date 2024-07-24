5dcc33b42bb3785ad45f4972e6844e99b4dfe4fb
YouSeungBeom <162657946+YouSeungBeom@users.noreply.github.com>
2024-07-25 00:34:13 +0900

    [level 0] Title: 문자열 뒤집기, Time: 15.00 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,9 +1,7 @@
 class Solution {
     public String solution(String my_string, int s, int e) {
-        String front = my_string.substring(0, s);
         StringBuilder middle = new StringBuilder(my_string.substring(s, e + 1));
-        String back = my_string.substring(e + 1, my_string.length());
         middle.reverse();
-        return front + middle + back;
+        return my_string.substring(0, s) + middle + my_string.substring(e + 1, my_string.length());
     }
 }
