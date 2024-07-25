c5d4ba7899510bdebfc87941f656638200afffce
YouSeungBeom <162657946+YouSeungBeom@users.noreply.github.com>
2024-07-25 18:50:46 +0900

    [level 2] Title: 짝지어 제거하기, Time: 140.26 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -4,19 +4,14 @@ class Solution {
     public int solution(String s) {
         Stack<Character> stack = new Stack<>();
         
-        for(int i = 0; i < s.length(); i++) {
-            if(stack.isEmpty()) {
-                stack.push(s.charAt(i));
-            }
-            else {
-                if(stack.peek() == s.charAt(i)) {
-                    stack.pop();
-                }
-                else {
-                    stack.push(s.charAt(i));
-                }
+        for (char c : s.toCharArray()) {
+            if (!stack.isEmpty() && stack.peek() == c) {
+                stack.pop();
+            } else {
+                stack.push(c);
             }
         }
+        
         return stack.isEmpty() ? 1 : 0;
     }
 }
