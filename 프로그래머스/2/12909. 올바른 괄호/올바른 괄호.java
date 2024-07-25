7bf521c24e27aa09cfbc1e3df076bda7a41105a2
YouSeungBeom <162657946+YouSeungBeom@users.noreply.github.com>
2024-07-25 17:17:23 +0900

    [level 2] Title: 올바른 괄호, Time: 7.85 ms, Memory: 53.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,23 @@
+class Solution {
+    boolean solution(String s) {
+        int count = 0;
+        boolean answer = false;
+        
+        for(int i = 0; i < s.length(); i++) {
+            if(count < 0) {
+                break;
+            }
+            if(s.charAt(i) == '(') {
+                count++;
+            } else if (s.charAt(i) == ')') {
+                count--;
+            }
+        }
+        
+        if(count == 0) {
+            answer = true;
+        }
+        
+        return answer;
+    }
+}
