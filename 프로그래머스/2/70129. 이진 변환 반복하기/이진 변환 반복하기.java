95308ec931ea97b24a53a73446d79fe64097edd1
YouSeungBeom <162657946+YouSeungBeom@users.noreply.github.com>
2024-07-25 17:53:50 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 8.09 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -2,10 +2,12 @@ class Solution {
     public int[] solution(String s) {
         int[] answer = new int[2];
         answer[0] = 1;                                  // 처음은 0회차가 아닌 1회차
+        
         while(!(s.equals("1"))) {
+            int len = s.length();
             if(s.contains("0")) {
-                s = s.replaceFirst("0", "");            // 0을 하나씩 제거해서 제거한 0의 개수 카운트
-                answer[1]++;
+                s = s.replace("0", "");
+                answer[1] += len - s.length();          // 처음 문자열에서 줄어든 길이만큼 0 제거
             } else {
                 s = Integer.toBinaryString(s.length()); // 길이를 2진수 문자열로 변환
                 answer[0]++;
