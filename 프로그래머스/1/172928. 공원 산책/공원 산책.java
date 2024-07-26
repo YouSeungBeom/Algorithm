f6c137741d86de7ebcc9a2b12ea7ed59f1747a53
YouSeungBeom <162657946+YouSeungBeom@users.noreply.github.com>
2024-07-26 15:24:58 +0900

    [level 1] Title: 공원 산책, Time: 16.92 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.java
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.java
@@ -3,6 +3,7 @@ class Solution {
         char[][] area = new char[park.length][park[0].length()];
         int w_idx = 0;
         int h_idx = 0;
+        
         // park를 area 2차원 배열에 기록. (좌표를 쉽게 추출하기 위함)
         for(int i = 0; i < park.length; i++) {
             for(int j = 0; j < park[i].length(); j++) {
@@ -13,16 +14,11 @@ class Solution {
                 }
             }
         }
-        for (int i = 0; i < area.length; i++) {
-            for (int j = 0; j < area[i].length; j++) {
-                System.out.print(area[i][j] + " ");
-            }
-            System.out.println();
-        }
+        
         // area의 가로 세로 길이 저장
         int W = park[0].length();
         int H = park.length;
-        System.out.println("[" + h_idx + ", " + w_idx + "]");
+        
         // 명령에 따른 움직임
         for(int i = 0; i < routes.length; i++) {
             boolean passable = true;
@@ -30,6 +26,7 @@ class Solution {
             int n = Integer.parseInt(routes[i].split(" ")[1]);
             System.out.println(op + " " + n);
             switch(op) {
+                    
                 case "E" :
                     if(w_idx + n < W) {
                         for(int j = w_idx; j <= w_idx + n; j++) {
@@ -42,8 +39,8 @@ class Solution {
                             w_idx += n;
                         }
                     }
-                    System.out.println("[" + h_idx + ", " + w_idx + "]");
                     break;
+                    
                 case "W" :
                     if(w_idx - n >= 0) {
                         for(int j = w_idx; j >= w_idx - n; j--) {
@@ -56,8 +53,8 @@ class Solution {
                             w_idx -= n;
                         }
                     }
-                    System.out.println("[" + h_idx + ", " + w_idx + "]");
                     break;
+                    
                 case "S" :
                     if(h_idx + n < H) {
                         for(int j = h_idx; j <= h_idx + n; j++) {
@@ -70,8 +67,8 @@ class Solution {
                             h_idx += n;
                         }
                     }
-                    System.out.println("[" + h_idx + ", " + w_idx + "]");
                     break;
+                    
                 case "N" :
                     if(h_idx - n >= 0) {
                         for(int j = h_idx; j >= h_idx - n; j--) {
@@ -84,10 +81,10 @@ class Solution {
                             h_idx -= n;
                         }
                     }
-                    System.out.println("[" + h_idx + ", " + w_idx + "]");
                     break;
             }
         }
+        
         // answer에 대입
         int[] answer = {h_idx, w_idx};
         return answer;
