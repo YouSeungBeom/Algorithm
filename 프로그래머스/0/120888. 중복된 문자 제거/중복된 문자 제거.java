0b7bd725da8de1c5dd43f12eff52158f1de36f09
YouSeungBeom <162657946+YouSeungBeom@users.noreply.github.com>
2024-07-27 01:58:46 +0900

    [level 0] Title: 중복된 문자 제거, Time: 1.12 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -2,16 +2,10 @@ import java.util.LinkedHashSet;
 
 class Solution {
     public String solution(String my_string) {
-        StringBuilder answer = new StringBuilder();
         LinkedHashSet<String> set = new LinkedHashSet<>();
-        
         for(String s : my_string.split("")) {
             set.add(s);
         }
-        for(String s : set) {
-            answer.append(s);
-        }
-        
-        return answer.toString();
+        return String.join("", set);
     }
 }
