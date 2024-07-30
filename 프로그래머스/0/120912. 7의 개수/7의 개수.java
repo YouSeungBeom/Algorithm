7cdfa5c0cd6dd4b9cb31869c807ec426c80af24c
YouSeungBeom <162657946+YouSeungBeom@users.noreply.github.com>
2024-07-30 19:55:30 +0900

    [level 0] Title: 7의 개수, Time: 11.13 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        StringBuilder sb = new StringBuilder();
+        for(int i : array) {
+            sb.append(i + "");
+        }
+        for(int i = 0; i < sb.length(); i++) {
+            if(sb.toString().charAt(i) == '7') {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
