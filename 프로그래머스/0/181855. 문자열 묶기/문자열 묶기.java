fede2b977e14b41500a06af94d761a55e03cda28
YouSeungBeom <162657946+YouSeungBeom@users.noreply.github.com>
2024-08-03 20:14:43 +0900

    [level 0] Title: 문자열 묶기, Time: 6.32 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
@@ -2,12 +2,12 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(String[] strArr) {
-        int[] arr = new int[100001];
+        int[] arr = new int[31];
         for(int i = 0; i < strArr.length; i++) {
             arr[strArr[i].length()]++;
         }
         Arrays.sort(arr);
         
-        return arr[100000];
+        return arr[30];
     }
 }
