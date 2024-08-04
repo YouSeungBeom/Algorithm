6903d9189f4fb9a15ef12c9483a7eca53c3dbe26
YouSeungBeom <162657946+YouSeungBeom@users.noreply.github.com>
2024-08-04 15:29:05 +0900

    [Silver IV] Title: 스택 2, Time: 1240 ms, Memory: 310092 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		Stack<Integer> stack = new Stack<>();
+		
+		for(int i = 0; i < N; i++) {
+			String[] str = br.readLine().split(" ");
+			
+			switch(str[0]) {
+				case "1" : 
+					stack.push(Integer.parseInt(str[1]));
+					break;
+				case "2" : 
+					if(stack.isEmpty()) {
+						bw.write("-1" + "\n");
+						break;
+					}
+					bw.write(Integer.toString(stack.pop()) + "\n");
+					break;
+				case "3" : 
+					bw.write(Integer.toString(stack.size()) + "\n");
+					break;
+				case "4" : 
+					if(stack.isEmpty()) {
+						bw.write("1" + "\n");
+						break;
+					}
+					bw.write("0" + "\n");
+					break;
+				case "5" : 
+					if(stack.isEmpty()) {
+						bw.write("-1" + "\n");
+						break;
+					}
+					bw.write(Integer.toString(stack.peek()) + "\n");
+					break;
+					
+			}
+		}
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
