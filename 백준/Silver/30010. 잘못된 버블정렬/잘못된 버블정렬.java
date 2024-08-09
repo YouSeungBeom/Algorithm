ae1e5e09a08d47966224b41a6c82deebb465bfbd
YouSeungBeom <162657946+YouSeungBeom@users.noreply.github.com>
2024-08-09 15:27:37 +0900

    [Silver V] Title: 잘못된 버블정렬, Time: 144 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Silver/30010. 잘못된 버블정렬/잘못된 버블정렬.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] A = new int[N];
+		
+		for(int i = 0; i < N; i++) {
+			for(int j = i; j < N; j++) {
+				A[i]++;
+			}
+		}
+		
+		A[N - 1] = 1;
+		A[N - 2] = 0;
+		
+		for(int i = 0; i < N; i++) {
+			bw.write(Integer.toString(A[i]) + " ");
+		}
+		
+		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
