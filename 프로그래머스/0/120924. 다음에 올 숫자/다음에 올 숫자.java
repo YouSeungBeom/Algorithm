28089f1dff190c6eaf7fe40bb507d4bb54df6138
YouSeungBeom <162657946+YouSeungBeom@users.noreply.github.com>
2024-08-15 14:08:53 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] common) {
+        int answer = 0;
+        if(common[1] * 2 == common[0] + common[2]) {
+            int d = common[1] - common[0];
+            answer = common[common.length - 1] + d;
+        }
+        else {
+            int r = common[1] / common[0];
+            answer = common[common.length - 1] * r;
+        }
+        return answer;
+    }
+}
