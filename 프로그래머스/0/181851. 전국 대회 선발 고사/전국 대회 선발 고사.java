e72e2d38c2fefd990b43081e855228ea31a4bcfb
YouSeungBeom <162657946+YouSeungBeom@users.noreply.github.com>
2024-08-20 16:19:28 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 2.78 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
@@ -1,23 +1,19 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 class Solution {
     public int solution(int[] rank, boolean[] attendance) {
         List<Integer> list = new ArrayList<>();
-        
-        for (int i = 0; i < attendance.length; i++) {
-            if (attendance[i]) {
-                list.add(i);
+        List<Integer> rank_list = Arrays.stream(rank).boxed().collect(Collectors.toList());
+        for(int i = 0; i < rank.length; i++) {
+            if(attendance[i]) {
+                list.add(rank[i]);
             }
         }
-        
-        list.sort(Comparator.comparingInt(i -> rank[i]));
-        
-        int a = list.get(0);
-        int b = list.get(1);
-        int c = list.get(2);
+        Collections.sort(list);
+        int a = rank_list.indexOf(list.get(0));
+        int b = rank_list.indexOf(list.get(1));
+        int c = rank_list.indexOf(list.get(2));
         
         return 10000 * a + 100 * b + c;
     }
