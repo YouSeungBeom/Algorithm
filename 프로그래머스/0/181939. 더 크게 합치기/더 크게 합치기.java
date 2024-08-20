24e97a4e9769a82345552f2d3d14ad11ae904ecf
YouSeungBeom <162657946+YouSeungBeom@users.noreply.github.com>
2024-08-20 15:33:06 +0900

    [level 0] Title: 더 크게 합치기, Time: 2.99 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,5 @@
+class Solution {
+    public int solution(int a, int b) {
+        return Integer.parseInt(Integer.toString(a) + Integer.toString(b)) >= Integer.parseInt(Integer.toString(b) + Integer.toString(a)) ? Integer.parseInt(Integer.toString(a) + Integer.toString(b)) : Integer.parseInt(Integer.toString(b) + Integer.toString(a));
+    }
+}
