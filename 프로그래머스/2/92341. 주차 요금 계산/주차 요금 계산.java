fb38a94682472e2c98df04074d4884f5587e1d36
YouSeungBeom <162657946+YouSeungBeom@users.noreply.github.com>
2024-08-23 13:01:03 +0900

    [level 2] Title: 주차 요금 계산, Time: 30.28 ms, Memory: 82.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] fees, String[] records) {
+        Set<Integer> set = new TreeSet<>();     // 차량번호 오름차순 저장 (중복 X)
+        int n = records.length;
+        int[][] arr = new int[n][3];
+        
+        // arr 배열에 분할저장
+        for(int i = 0; i < n; i++) {
+            String[] r = records[i].split(" ");
+            
+            // 시간을 분으로 변환해서 저장
+            arr[i][0] = Integer.parseInt(r[0].split(":")[0]) * 60 + Integer.parseInt(r[0].split(":")[1]);
+            arr[i][1] = Integer.parseInt(r[1]);
+            set.add(arr[i][1]);
+            arr[i][2] = r[2].equals("IN") ? 1 : 0;      // IN OUT 구분
+        }
+        
+        int count = set.size();                         // 차량 개수
+        int[] answer = new int[count];
+        List<Integer> car_nums = new ArrayList<>(set);      // 인덱스를 이용해 set의 요소를 불러오기 위함.
+            
+        for(int i = 0; i < count; i++) {
+            int time = 0;                               // 누적 주차시간
+            boolean IN = false;                         // 주차장에 차의 유무 판단
+            
+            for(int j = 0; j < n; j++) {
+                if(car_nums.get(i) == arr[j][1]) {          // 차 번호가 같을 경우 (오름차순)
+                    if(arr[j][2] == 1) {                // 입차한 경우
+                        time -= arr[j][0];
+                        IN = true;
+                    }
+                    else {                              // 출차한 경우
+                        time += arr[j][0];
+                        IN = false;
+                    }
+                    answer[i] = time;
+                }
+            }
+            if(IN) {                                    // 입차는 했지만, 출차 기록이 없을 경우
+                answer[i] += 1439;
+            }
+        }
+        
+        for(int i = 0; i < count; i++) {
+            answer[i] -= fees[0];
+            if(answer[i] <= 0) {
+                answer[i] = fees[1];
+            }
+            else {
+                if(answer[i] % fees[2] == 0) {
+                    answer[i] = fees[1] + (answer[i] / fees[2]) * fees[3];
+                }
+                else {
+                    answer[i] = fees[1] + (answer[i] / fees[2] + 1) * fees[3];
+                }
+            }
+        }
+        return answer;
+    }
+}
