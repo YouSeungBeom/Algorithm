4be7d4f88cba28fab14009ce1cb32b919938b133
YouSeungBeom <162657946+YouSeungBeom@users.noreply.github.com>
2024-10-07 20:41:23 +0900

    [level 1] Title: 숫자 짝꿍, Time: 103.62 ms, Memory: 150 MB -BaekjoonHub

---
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String solution(String X, String Y) {
+        int[] K_x = new int[10];
+        int[] K_y = new int[10];
+        StringBuilder sb = new StringBuilder();
+        
+        for(char c : X.toCharArray()) {
+            K_x[c - '0']++;
+        }
+        for(char c : Y.toCharArray()) {
+            K_y[c - '0']++;
+        }
+        
+        for(int i = 9; i >= 0; i--) {
+            if(K_x[i] == 0 || K_y[i] == 0) 
+                continue;
+            for(int j = 0; j < Math.min(K_x[i], K_y[i]); j++) {
+                sb.append(i);
+            }
+        }
+        toZero(sb);
+        return sb.length() == 0 ? "-1" : sb.toString();
+    }
+    
+    void toZero (StringBuilder s) {
+        if(s.toString().matches("0+")) {
+            s.setLength(0);
+            s.append(0);
+        }
+    }
+}
