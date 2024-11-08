07763fef56f250bf67cc806fbeca8c8d7f18b711
YouSeungBeom <162657946+YouSeungBeom@users.noreply.github.com>
2024-11-08 09:53:38 +0900

    [level 0] Title: 배열 만들기 2, Time: 672.69 ms, Memory: 407 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -1,28 +1,30 @@
+import java.util.List;
 import java.util.ArrayList;
 
 class Solution {
     public int[] solution(int l, int r) {
-        ArrayList<Integer> list = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        
         for(int i = l; i <= r; i++) {
-            String num = Integer.toString(i);
-            boolean is05 = true;
-            for(int j = 0; j < num.length(); j++) {
-                if(num.charAt(j) != '0' && num.charAt(j) != '5') {
-                    is05 = false;
-                    break;
-                }
+            String s = Integer.toString(i);
+            if(!(s.contains("5") || s.contains("0"))) {
+                continue;
             }
-            if(is05)
+            s = s.replaceAll("5", "");
+            s = s.replaceAll("0", "");
+            if(s.length() == 0)
                 list.add(i);
         }
         if(list.size() == 0) {
             int[] answer = {-1};
             return answer;
         }
-        int[] answer = new int[list.size()];
-        for(int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i);
+        else {
+            int[] answer = new int[list.size()];
+            for(int i = 0; i < answer.length; i++) {
+                answer[i] = list.get(i);
+            }
+            return answer;
         }
-        return answer;
     }
 }
