b936cffe7658897de4a7432d12c4ac1ba747204a
YouSeungBeom <162657946+YouSeungBeom@users.noreply.github.com>
2024-11-08 11:16:36 +0900

    [level 0] Title: 배열 만들기 4, Time: 57.59 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -1,28 +1,15 @@
-import java.util.ArrayList;
+import java.util.Stack;
 
 class Solution {
     public int[] solution(int[] arr) {
-        ArrayList<Integer> list = new ArrayList<>();
-        int i = 0;
-        while (i < arr.length) {
-            if (list.isEmpty()) {
-                list.add(arr[i]);
-                i++;
-            } 
-            else {
-                if (list.get(list.size() - 1) < arr[i]) {
-                    list.add(arr[i]);
-                    i++;
-                } 
-                else {
-                    list.remove(list.size() - 1);
-                }
+        Stack<Integer> stack = new Stack<>();
+        
+        for(int i : arr) {
+            while(!stack.isEmpty() && stack.peek() >= i) {
+                stack.pop();
             }
+            stack.push(i);
         }
-        int[] stk = new int[list.size()];
-        for(int a = 0; a < list.size(); a++) {
-            stk[a] = list.get(a);
-        }
-        return stk;
+        return stack.stream().mapToInt(i -> i).toArray();
     }
 }
