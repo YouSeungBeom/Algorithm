0042a2b08874f4c3b73519a907d8f0db6d0995ec
YouSeungBeom <162657946+YouSeungBeom@users.noreply.github.com>
2024-11-22 00:31:43 +0900

    [level 0] Title: 배열 만들기 5, Time: 1894.91 ms, Memory: 404 MB -BaekjoonHub

--- 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
@@ -1,16 +1,19 @@
-import java.util.ArrayList;
-
 class Solution {
     public int[] solution(String[] intStrs, int k, int s, int l) {
-        ArrayList<Integer> list = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        int cnt = 0;
+        
         for(int i = 0; i < intStrs.length; i++) {
-            if(Integer.parseInt(intStrs[i].substring(s, s + l)) > k) {
-                list.add(Integer.parseInt(intStrs[i].substring(s, s + l)));
+            int tmp = Integer.parseInt(intStrs[i].substring(s, s + l));
+            
+            if(tmp > k) {
+                sb.append(Integer.toString(tmp) + " ");
+                cnt++;
             }
         }
-        int[] answer = new int[list.size()];
-        for(int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i);
+        int[] answer = new int[cnt];
+        for(int i = 0; i < cnt; i++) {
+            answer[i] = Integer.parseInt(sb.toString().split(" ")[i]);
         }
         return answer;
     }
