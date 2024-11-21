abfe476bfd2e4baf3f6059bfd6ae84834ebb3778
YouSeungBeom <162657946+YouSeungBeom@users.noreply.github.com>
2024-11-22 01:10:09 +0900

    [Bronze V] Title: A+B, Time: 92 ms, Memory: 12884 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner stdin = new Scanner(System.in);
+		
+		int A = stdin.nextInt();
+		int B = stdin.nextInt();
+		
+		System.out.print(A+B);
+	}
+}
